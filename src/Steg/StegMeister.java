2fbacda0c5b53c731174c8d61bdcf2e828b7fb43
GaleForce <ZachGale89@gmail.com>
2018-02-06 13:58:00 -0600

    Initial commit

---
+++ src/Steg/StegMeister.java
@@ -0,0 +1,25 @@
+package Steg;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class StegMeister extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("UI.fxml"));
+        primaryStage.setTitle("Steg");
+        primaryStage.getIcons().add(new Image(StegMeister.class.getResourceAsStream("icon.png")));
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
