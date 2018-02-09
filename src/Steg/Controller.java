0c4fc75f2bdf6f7eec9b6dc1029219ea0240a704
GaleForce <ZachGale89@gmail.com>
2018-02-08 20:21:03 -0600

    Updated the ui for better testing, added fields and buttons. Started on encryption and have so far only came up with an idea and need to work on it. TODO: optimize imports at the end.
    Signed-off-by: GaleForce <ZachGale89@gmail.com>

--- src/Steg/Controller.java
+++ src/Steg/Controller.java
@@ -1,4 +1,36 @@
 package Steg;
 
+//Import required packages for javafx
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+
 public class Controller {
+
+    @FXML private javafx.scene.control.TextArea test1; //text areas
+    @FXML private javafx.scene.control.TextArea test2;
+    @FXML private javafx.scene.control.TextArea test3;
+    @FXML private javafx.scene.control.TextField test1input; //inputs
+    @FXML private javafx.scene.control.TextField test2input;
+    @FXML private javafx.scene.control.TextField test3input;
+
+    @FXML
+    public void test1btn(){
+
+        //test1.setText(test1input.getText()); //Test to see if ui functions and it does
+        EnCrypt cryptotest = new EnCrypt(); //create a new encryption object
+
+        String msg = "Hopeitworks"; //the message
+        String key = "don'tfailmenopwAES"; //our key
+        String sMsg;
+
+        sMsg = cryptotest.Crypt(msg, key);
+
+        test1.setText(sMsg);
+
+
+    }
+
+
 }
