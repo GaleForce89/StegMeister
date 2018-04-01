5b75a78ad3b3f045b1d92e2c3e3d5cc1c4e02378
Chris <35858311+cawaterman91@users.noreply.github.com>
2018-03-31 19:25:48 -0500

    Partially complete encrypt function. Everything involving the actual encryption is correct, but we need to change how the encrypted byte array is stored. Currently the output is not meaningful because it uses the toString function to try to read the byte array into a string, which does not work as intended.

--- src/main/java/Steg/Cryptography/Encrypt.java
+++ src/main/java/Steg/Cryptography/Encrypt.java
@@ -6,6 +6,12 @@ import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
+import java.security.SecureRandom;
+import javax.crypto.spec.IvParameterSpec;
+import java.util.Arrays;
+import java.lang.StringBuffer;
+
+
 
 /** The encrypt class is used for all things encryption, to include key generation. */
 public class Encrypt extends Cryptography {
@@ -15,24 +21,39 @@ public class Encrypt extends Cryptography {
     super();
   }
 
-  /** Default constructor to null */
-  // public Encrypt() throws NoSuchAlgorithmException, NoSuchPaddingException {
-  //     super();
-  // }
-  public String Crypt(String msg, String test) {
+  public String Crypt(String msg, String test) { //DOES NOT WORK, NEED TO CORRECTLY CONVERT BYTE ARRAY TO STRING
     /*
     Heavy work in progress, going to take in strings, create more methods and convert the strings to bytes,
     from there at the end convert bytes to strings to keep things easy on the ui side.
      */
 
-    try { // lets try to create the cipher and hope it works!
+    //try using cipher block chaining, need to create a random initialization vector so ciphertext is different
+      // with each encryption
+    byte[] initVector = new byte[16]; //create byte array to fill initialization vector
+    SecureRandom randomNumber = new SecureRandom(); //new random number to randomize initialization vector
+    randomNumber.nextBytes(initVector); //fill byte array with random numbers
+    IvParameterSpec ivpspec = new IvParameterSpec(initVector); //set up initialization vector with random byte array
 
+    //save init vector to a file/db?
+
+    //generate and set the random key
+    setKey(genKey(keySize));
+    StringBuilder strBuilder = new StringBuilder();
+
+    try {
+        //set up the Cipher object with the secret key and the initialization vector
+        stego.init(Cipher.ENCRYPT_MODE, key, ivpspec);
+        byte[] in = msg.getBytes("UTF-8"); //put message in byte array
+        byte[] out = stego.doFinal(in); //encrypt message using Cipher object and store in byte array
+        strBuilder.append(Arrays.toString(out)); //*****this doesn't work, look into using base64 encoder for conversion*****
     } catch (Exception ex) { // catch if we run into issues.
       ex.printStackTrace();
+        return "Something went terribly wrong..... contact support"; // display something went wrong if
+        // it does
     }
+    return strBuilder.toString();//change this if we implement base64 encoding
 
-    return "Something went terribly wrong..... contact support"; // display something went wrong if
-    // it does
+
   }
 
   /**
