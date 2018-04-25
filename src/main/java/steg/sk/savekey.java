1af855bb514123fcb3c5e2ab16121bccd6f7deea
Riley <rreeder2@leomail.tamuc.edu>
2018-04-24 21:24:26 -0500

    Still can't seem to get the datbase key and keyword variables

---
+++ src/main/java/steg/sk/savekey.java
@@ -0,0 +1,33 @@
+package steg.sk;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
+public class savekey
+{
+
+    public static void main(String[] args)
+    {
+        steg.database.InsertData needKey = new steg.database.InsertData();
+
+        String key = needKey.insert_Key(key);
+        String keyword = needKey.insert_Key(keyword);
+
+        try
+        {
+            BufferedWriter w = new BufferedWriter ( new FileWriter(".\\text.txt") );
+
+            w.write(key);
+            w.write(","); //when reading in file the "," will help distinguish end of key
+            w.write(keyword);
+
+            w.close();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
