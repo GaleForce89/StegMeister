d03aa43ee3fdeb4836800083a5e273c37ed0c643
Riley <rreeder2@leomail.tamuc.edu>
2018-04-28 21:58:12 -0500

    h

--- src/main/java/steg/io/SaveKey.java
+++ src/main/java/steg/io/SaveKey.java
@@ -11,7 +11,7 @@ import java.sql.SQLException;
 
 public class SaveKey
 {
-    //**Connect to database
+    //**Connect to database*/
     private Connection connect() {
         String url = "jdbc:sqlite:DB/dbKeys.db";
         Connection conn = null;
@@ -25,25 +25,25 @@ public class SaveKey
 
     public void GetKey(String[] args)
     {
-        //**sql for get key and keyword from database
+        //**sql for get key and keyword from database*/
         String sqlKey = "SELECT * FROM keys WHERE key";
         String sqlKeyword = "SELECT * FROM key WHERE keyword";
 
 
         try (Connection conn = this.connect())
         {
-            //**bufferedWriter and PreparedStatement
+            //**bufferedWriter and PreparedStatement*/
             BufferedWriter w = new BufferedWriter ( new FileWriter(".\\text.txt") );
             PreparedStatement psKey = conn.prepareStatement(sqlKey);
             PreparedStatement psKeyword = conn.prepareStatement(sqlKeyword);
 
-            //**get info from table and set as variables
+            //**get info from table and set as variables*/
             int key = psKey.executeUpdate();
             int keyword = psKeyword.executeUpdate();
 
-            //**print variables
+            //**print variables*/
             w.write(key);
-            w.write(","); //when reading in file the "," will help distinguish end of key
+            w.write(","); //**when reading in file the "," will help distinguish end of key*/
             w.write(keyword);
 
             w.close();
