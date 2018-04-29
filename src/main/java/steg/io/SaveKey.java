bf9d4a2f9c02010bc05f71653be4c943903edc54
Riley <rreeder2@leomail.tamuc.edu>
2018-04-28 23:06:52 -0500

    A

--- src/main/java/steg/io/SaveKey.java
+++ src/main/java/steg/io/SaveKey.java
@@ -8,23 +8,23 @@ import steg.cryptography.Cryptography;
 
 public class SaveKey /**SaveKey Method*/
 {
-    public void GetKey(String[] args)//a
+    public void GetKey(String[] args)
     {
         Cryptography crypt = new Cryptography();
 
         //Set variables
         String key = crypt.getKeyStr();
-        String keyword = crypt.getKeyword();
+        String keyword = crypt.getVector();
 
         try
         {
             //bufferedWriter and PreparedStatement
-            BufferedWriter w = new BufferedWriter ( new FileWriter(".\\text.txt") );
+            BufferedWriter w = new BufferedWriter ( new FileWriter(".\\text.key") );
 
 
             //print variables
             w.write(key);
-            w.write(","); //when reading in file the "," will help distinguish end of key
+            w.write("\n"); //when reading in file the "," will help distinguish end of key
             w.write(keyword);
 
             w.close();
