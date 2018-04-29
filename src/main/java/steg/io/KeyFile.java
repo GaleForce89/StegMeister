5f0b4b88dc015584f3acca1eb9661732cf5fc983
Riley <rreeder2@leomail.tamuc.edu>
2018-04-28 23:38:38 -0500

    B

--- src/main/java/steg/io/KeyFile.java
+++ src/main/java/steg/io/KeyFile.java
@@ -1,14 +1,14 @@
 package steg.io;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+
 import steg.cryptography.Cryptography;
 
-/**SaveKey Method*/
-public class SaveKey
+/**KeyFile Class*/
+public class KeyFile
 {
-    public void GetKey()
+    /*rightKey Method*/
+    public void rightKey()
     {
         Cryptography crypt = new Cryptography();
 
@@ -33,5 +33,21 @@ public class SaveKey
         {
             e.printStackTrace();
         }
+    }
+
+    /*loadKey Method*/
+    public void loadKey()
+    {
+        //Call Cryptography
+        Cryptography crypt = new Cryptography();
+        //Make BufferedReader and FileReader
+        BufferedReader w = new BufferedReader ( new FileReader(".\\text.key") );
+
+        //Make and send Variables
+        String key = w.readLine();
+        crypt.setKey(key);
+        String keyword = w.readLine();
+        crypt.setKeyword(keyword);
+
     }
 }
