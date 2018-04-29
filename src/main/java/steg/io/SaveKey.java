dd863b04630eeba1d28bf55b680adfa8aaedaf9d
Riley <rreeder2@leomail.tamuc.edu>
2018-04-28 22:16:35 -0500

    h

--- src/main/java/steg/io/SaveKey.java
+++ src/main/java/steg/io/SaveKey.java
@@ -3,55 +3,34 @@ package steg.io;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import steg.cryptography.Cryptography;
 
 
-public class SaveKey
+public class SaveKey /**SaveKey Method*/
 {
-    /**Connect to database*/
-    private Connection connect() {
-        String url = "jdbc:sqlite:DB/dbKeys.db";
-        Connection conn = null;
-        try {
-            conn = DriverManager.getConnection(url);
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-        return conn;
-    }
-
     public void GetKey(String[] args)
     {
-        /**sql for get key and keyword from database*/
-        String sqlKey = "SELECT * FROM keys WHERE key";
-        String sqlKeyword = "SELECT * FROM key WHERE keyword";
+        Cryptography crypt = new Cryptography();
 
+        //Set variables
+        String key = crypt.getKeyStr();
+        String keyword = crypt.getKeyword();
 
-        try (Connection conn = this.connect())
+        try
         {
-            /**bufferedWriter and PreparedStatement*/
+            //bufferedWriter and PreparedStatement
             BufferedWriter w = new BufferedWriter ( new FileWriter(".\\text.txt") );
-            PreparedStatement psKey = conn.prepareStatement(sqlKey);
-            PreparedStatement psKeyword = conn.prepareStatement(sqlKeyword);
 
-            //**get info from table and set as variables*/
-            int key = psKey.executeUpdate();
-            int keyword = psKeyword.executeUpdate();
 
-            /**print variables*/
+            //print variables
             w.write(key);
-            w.write(","); /**when reading in file the "," will help distinguish end of key*/
+            w.write(","); //when reading in file the "," will help distinguish end of key
             w.write(keyword);
 
             w.close();
         }
         catch (IOException e)
         {
-            e.printStackTrace();
-        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
