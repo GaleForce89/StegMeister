cb83c320cf285423eb554bda53122080bb3f2693
GaleForce <GaleForce089@gmail.com>
2018-05-01 21:00:52 -0500

    Changed the connect class to sql. The name seemed more appropriate as our database is targeting sql. Also went ahead and optimized the database a bit and we now only have 1 connection which solves the huge issue of locking up.

--- src/main/java/steg/steganography/Model.java
+++ src/main/java/steg/steganography/Model.java
@@ -1,5 +1,11 @@
 package steg.steganography;
 
+/**
+ * All credit for this file belongs to
+ * @author Almas Baimagambetov (almaslvl@gmail.com)
+ * github: https://github.com/AlmasB/FXTutorials/tree/master/src/com/almasb/steg
+ */
+
 public class Model {
   public BasicEncoder encoder;
   public BasicDecoder decoder;
